2046faad95ec8e16f668713fcd006bb74410bc15
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-11-08 01:45:17 +0600

    create reduser

--- jd/script.js
+++ jd/script.js
@@ -1,1 +1,42 @@
-console.log("hello world");
+// Get UI  data 
+const counter = document.getElementById("counter");
+const Increment = document.getElementById("increment");
+const decrement = document.getElementById("decrement");
+
+
+// Initialize State 
+const InitialState = {
+    counter: 0,
+}
+
+function createReducer(state = InitialState, action) {
+    if (action.type === "INCREMENT") {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+
+    } else if (action.type === "DECREMENT") {
+        return {
+            ...state,
+            counter: state.counter - 1
+        }
+    } else {
+        return state;
+    }
+}
+
+
+// Create Store
+
+const store = createStore(createReducer,);
+
+// Increment
+Increment.addEventListener("click", () => {
+    store.dispatch({
+        type: "INCREMENT"
+
+    })
+    
+}
+)
